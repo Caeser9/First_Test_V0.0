a87023c52c7b0698ce9c7b58e7c400a38eabe74e
Kaycer Khouini <81649901+Caeser9@users.noreply.github.com>
2022-01-18 18:10:29 +0100

    Update TokenizerMapper.java

--- TokenizerMapper.java
+++ TokenizerMapper.java
@@ -19,16 +19,20 @@ public class TokenizerMapper extends Mapper<Object, Text, Text, DoubleWritable>{
         } return false;
     }
     private Text word = new Text();
-    public void map(Object key, Text value, Mapper.Context context ) throws IOException, InterruptedException {
-        String s1=value.toString().toString().trim().replaceAll(" +", "\t");
+    public void map(Object key, Text value, Mapper.Context context )
+            throws IOException, InterruptedException {
+        String s1=value.toString().toString().trim().replaceAll(" ,", "\t");
         System.out.println("s1="+s1); StringTokenizer itr = new StringTokenizer(s1);
         String Influencer="";
         String v="";
         Double shares=0.0;
         int i=0;
         while (itr.hasMoreTokens()) {
-            v=itr.nextToken(); if (i==2){
-                Influencer=v.toString();} if (i>2 && isNumeric(v.toString())){
+            v=itr.nextToken();
+            if (i==2){
+                Influencer=v.toString();
+            }
+            if (i>2 && isNumeric(v.toString())){
                 shares=Double.parseDouble(v.toString()); context.write(new Text(Influencer), new DoubleWritable(shares));
             }
             System.out.println("Influencer="+Influencer+"Numero de Shares"+shares);
